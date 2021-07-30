4b679aae05405dd770177639647640572cb645cf
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-30 11:25:57 -0500

    add pass test for #== method on project

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/project')
+require('./lib/volunteer')
 require('pry')
 also_reload('lib/**/*.rb')
 require 'pg'
 
-DB = PG.connect({:dbname => "project"})
+DB = PG.connect({:dbname => "volunteer_tracker"})
 
--- lib/project.rb
+++ lib/project.rb
@@ -1,2 +1,20 @@
 require('pry')
 
+class Project
+attr_reader :id 
+attr_accessor :title 
+
+def initialize(attributes)
+  @title = attributes.fetch(:title)
+  @id = attributes.fetch(:id)
+end 
+
+def ==(compare_title)
+  # binding.pry
+  self.title == (compare_title.title)
+end 
+
+
+end 
+
+
