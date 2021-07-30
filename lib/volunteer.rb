0191ed742c35eaa63a2374849bf72ee66cf2e2cb
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-30 13:12:46 -0500

    add name method to volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,1 +1,18 @@
 require('pry')
+class Volunteer
+attr_reader :id, :project_id
+attr_accessor :name, :hours
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @hours = attributes.fetch(:hours)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+  
+  def name
+    @name
+  end 
+
+
+end 
